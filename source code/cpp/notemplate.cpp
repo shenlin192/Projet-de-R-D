73f6d6e49d64c705310de1293b8856abe41b9d03
shenlin <shenlin192@gmail.com>
2016-11-13 00:18:50 +0100

    clean some code

--- source code/cpp/notemplate.cpp
+++ source code/cpp/notemplate.cpp
@@ -1,27 +1,9 @@
-#include <DGtal/base/Common.h>
+
 
 #include <iostream>
 #include <sstream>
-#include "DGtal/base/Common.h"
-#include "DGtal/helpers/StdDefs.h"
-
-#include "DGtal/io/boards/Board2D.h"
-
-#include "DGtal/geometry/curves/ArithmeticalDSL.h"
 
 using namespace std;
-using namespace DGtal;
-
-
-/*void exampleNaiveDSL()
-{
-  trace.beginBlock ( "Naive DSL" );
-  using namespace Z2i; 
-  // Construct a naive DSL from a, b, mu
-  NaiveDSL<Integer> line( 2, 5, 0 ); 
-	Point bottomLeft( 0, 0 );
-}*/
-
 
 //definition of a class
 class Rectangle {
@@ -30,11 +12,11 @@ class Rectangle {
 
   public:
     Rectangle (int,int);
-    Rectangle(Rectangle& r);
+    //Rectangle(Rectangle& r);
     Rectangle(){};
     int area();
     int perimeter();
-    //static int addition (int a, int b)
+
 };
 
 //definition of member functions
@@ -42,11 +24,11 @@ Rectangle::Rectangle (int a, int b) {
 	  width = a;
 	  height = b;
 }
-
+/*
 Rectangle::Rectangle(Rectangle& r) {
     width = r.width;
     height = r.height;
-}
+}*/
 
 int Rectangle::area() {
     return width * height;
@@ -56,45 +38,22 @@ int Rectangle::perimeter() {
     return 2 * width + 2 * height;
 }
 
-//a normal global function
-int addition (int a, int b)
-{
-  int r;
-  r=a+b;
-  return r;
-}
-
 //a normal global function that uses the Rectangle class 
-int creatRect(int a, int b){
+int calculate(int a, int b){
   Rectangle r1(a,b);
   return r1.area() + r1.perimeter();
 }
 
 //a global function that return a Rectangle object
-/*Rectangle createRect2(int a, int b){
+Rectangle createRect2(int a, int b){
   Rectangle r1(a,b);
   return r1;
-}*/
+}
 
 //a global function that return a Rectangle object
-Rectangle* createRect3(int a, int b){
+Rectangle* createRectPtr3(int a, int b){
   return new Rectangle(a,b);
 }
 
-//a main function
-/*
-int main(int argc, char** argv)
-{
-  
- // DGtal::trace.info() << "Helloworld from DGtal ";
- //DGtal::trace.emphase() << "(version "<< DGTAL_VERSION << ")"<< std::endl;
- Rectangle objet;
- objet=createRect2(3,4);
- cout<<objet.area()<<endl;
-	
- //cout<<rect.area()<<endl;
- //cout<<"hi33"<<endl;
-	return 0;
-}
-*/
+
 
