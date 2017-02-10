bd659f7dc4bf6b7436bbb559d82faddae58761d8
Nicolas Normand <Nicolas.J.Normand+github@gmail.com>
2017-02-10 17:34:33 +0100

    Using DGtal point constructor

--- sourceCode/2DPoints/2DPoints.js
+++ sourceCode/2DPoints/2DPoints.js
@@ -14,6 +14,7 @@ var PointStruct = Struct({
 });
 
 var PointStructRef = ref.refType(PointStruct);
+var intRef = ref.refType('int');
 
 var Dgtal = ffi.Library('../../build/2DPoints/lib/lib2DPointsLib', {
     // _ZNSt15binary_functionIN5DGtal11PointVectorILj2EiSt5arrayIiLm2EEEES4_dEC1Ev: [
@@ -21,15 +22,15 @@ var Dgtal = ffi.Library('../../build/2DPoints/lib/lib2DPointsLib', {
     // ],
 
     _Z13create2DPointii: [PointStructRef, ['int', 'int']], //ok
-    _Z11draw2DPointN5DGtal11PointVectorILj2EiSt5arrayIiLm2EEEES3_: [
+    _Z11draw2DPointN5DGtal11PointVectorILj2EiNSt3__15arrayIiLm2EEEEES4_: [
         'void', [PointStructRef, PointStructRef],
     ], //ok
 
     //Dgtal 2D point constructor
     //SyntaxError: Unexpected identifier
-    // _ZN5DGtal11PointVectorILj2EiSt5arrayIiLm2EEEC1ERKiS5_: [
-    //       PointStructRef, [PointStructRef, 'int', 'int']
-    // ] fail
+    _ZN5DGtal11PointVectorILj2EiNSt3__15arrayIiLm2EEEEC1ERKiS6_: [
+        PointStructRef, [PointStructRef, intRef, intRef],
+    ], // fail
 
 });
 
@@ -42,14 +43,17 @@ console.log(p1); // <Buffer@0x38c49b0 03 00 00 00 04 00 00 00>
 let p2 = new PointStruct();
 p2.x = 1;
 p2.y = 2;
-console.log(p2);
 // { x: 1,
 //   y: 2,
 //   'ref.buffer': <Buffer@0x38aab70 01 00 00 00 02 00 00 00> }
-Dgtal._Z11draw2DPointN5DGtal11PointVectorILj2EiSt5arrayIiLm2EEEES3_(p1, p2.ref());
+Dgtal._Z11draw2DPointN5DGtal11PointVectorILj2EiNSt3__15arrayIiLm2EEEEES4_(p1, p2.ref());
+console.log('p2: ', p2);
 
 
 // 3 Using the constructor of DGtal
-// var p3 = new PointStruct();
-// Dgtal._ZN5DGtal11PointVectorILj2EiSt5arrayIiLm2EEEC1ERKiS5_(p3.ref(), 4, 5)
-// console.log(p3)
+let p3 = new PointStruct();
+let i1 = ref.alloc(ref.types.int, 2);
+let i2 = ref.alloc(ref.types.int, 9);
+Dgtal._ZN5DGtal11PointVectorILj2EiNSt3__15arrayIiLm2EEEEC1ERKiS6_(p3.ref(), i1, i2);
+console.log('p3: ', p3);
+
